dab180c53b95e4b4e6ae352819aa2e92dc1fa34f
Barry Joyce <barryjohnjoyce@gmail.com>
2016-11-07 14:27:25 +0000

    First Commit

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+
+from django.utils import timezone
+
+
+# Create your models here.
+
+
+class AccountUserManager (UserManager):
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        """creates and saves a user with the given username, email and password"""
+
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given username must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email, is_staff=is_staff, is_active=True, is_superuser=is_superuser, date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractUser):
+    #Now that we've abstracted this class we can add any number of customer attribute to our user class.
+    #In later units we'll be adding things like payment details.
+
+    objects = AccountUserManager()
